46e7d5d358ac63625dca451c94e2ea6336cacc19
Abhishek Singh <abhishek2001singh2001@gmail.com>
2024-12-30 20:58:33 +0530

    CSS

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -16,6 +16,7 @@ class PredictPipeline:
             preprocessor = load_object(file_path=preprocessor_path)
             data_scaled=preprocessor.transform(features)
             preds = model.predict(data_scaled)
+
             return preds
        
         except Exception as e:
